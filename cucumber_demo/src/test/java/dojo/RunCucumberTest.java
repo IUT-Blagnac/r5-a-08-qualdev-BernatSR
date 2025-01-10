b6ce079e8d7a4da1e5db6973b42c610b7ed0fa9d
Bernat <65407864+BernatSR@users.noreply.github.com>
2025-01-10 16:04:03 +0100

    tp4

--- cucumber_demo/src/test/java/dojo/RunCucumberTest.java
+++ cucumber_demo/src/test/java/dojo/RunCucumberTest.java
@@ -1,7 +1,9 @@
 package dojo;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)  
+@CucumberOptions(plugin = {"json:target/cucumber.json"} )
 public class RunCucumberTest { }
